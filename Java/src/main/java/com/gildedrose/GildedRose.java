b4ced6ddb20d4517813bbfa3a16241b0fd4dcae1
Esguerra <Adrian.Esguerra@nchcss.com>
2018-09-21 17:58:54 +0800

    first commit

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -2,11 +2,12 @@ package com.gildedrose;
 
 class GildedRose {
     Item[] items;
+    final int MAX_QUALITY_VALUE = 50;
 
     public GildedRose(Item[] items) {
         this.items = items;
     }
-
+    
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
@@ -22,15 +23,11 @@ class GildedRose {
 
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                            increaseQuality(items[i]);
                         }
 
                         if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                            increaseQuality(items[i]);
                         }
                     }
                 }
@@ -52,11 +49,15 @@ class GildedRose {
                         items[i].quality = items[i].quality - items[i].quality;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+                    increaseQuality(items[i]);
                 }
             }
+        }
+    }
+
+    private void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
         }
     }
 }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -5,13 +5,156 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class GildedRoseTest {
+    final int MAX_QUALITY_VALUE = 50;
+
+    private Item[] itemsArray = {
+        new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20)
+    };
+
+    private boolean isPositive(int number) {
+        System.out.print("number: "+number);
+        System.out.println("result: "+(number>=0));
+        return number >= 0;
+    }
+
+    private boolean confirmDecreaseInValue(int start, int end) {
+        System.out.println("start: "+start);
+        System.out.println("end: "+end);
+        return start > end;
+    }
+    
+    private int[] getStartingQuality(Item[] items) {
+        int[] startingQuality = new int[items.length];
+        for(int i = 0; i < items.length; i++) {
+            startingQuality[i] = items[i].quality;
+        }
+        return startingQuality;
+    }
+
+    private int[] getStartingSellIn(Item[] items) {
+        int[] startingSellIn = new int[items.length];
+        for(int i = 0; i < items.length; i++) {
+            startingSellIn[i] = items[i].sellIn;
+        }
+        return startingSellIn;
+    }
+
+    public int getDifference(int start, int end) {
+        System.out.print("start: "+start);
+        System.out.println("end: "+end);
+        return start - end;
+    }
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void qualityIsNeverNegative() {
+        Item[] items = new Item[] { new Item("foo", 0, 20) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertTrue(isPositive(app.items[0].quality));
+    }
+
+    @Test
+    public void itemsHaveSellInValue() {
+        Item[] items = new Item[] { new Item("foo", 5, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        for(Item item : items) {
+            assertTrue(isPositive(item.sellIn));
+        }
+    }
+
+    @Test
+    public void itemsHaveQualityValue() {
+        Item[] items = new Item[] { new Item("foo", 5, 20) };
+        GildedRose app = new GildedRose(itemsArray);
+        app.updateQuality();
+        for(Item item : app.items) {
+            assertTrue(isPositive(item.quality));
+        }
+    }
+
+    @Test
+    public void lowersBothValues() {
+        Item[] items = new Item[] { new Item("foo", 0, 20) };
+        int[] startingQuality = getStartingQuality(items);
+        int[] startingSellIn = getStartingSellIn(items);
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        for (int i=0; i < app.items.length; i++) {
+            assertTrue(confirmDecreaseInValue(startingQuality[i], app.items[i].quality));
+            assertTrue(confirmDecreaseInValue(startingSellIn[i], app.items[i].sellIn));
+        }
+    }
+
+    @Test
+    public void overSellInDateAndDoubleDegradation() {  
+        Item[] items = new Item[] { new Item("foo", 0, 20) };
+        int[] startingQuality = getStartingQuality(items);
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        for (int i=0; i < app.items.length; i++) {
+            assertFalse(isPositive(app.items[i].sellIn));
+            assertEquals(2, getDifference(startingQuality[i], app.items[i].quality));
+        }
+    }
+
+    @Test
+    public void agedBrieHandler() {
+        Item[] items = new Item[] { new Item("Aged Brie", 5, 20) };
+        int[] startingQuality = getStartingQuality(items);
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        for (int i=0; i < app.items.length; i++) {
+            assertEquals("Aged Brie", app.items[i].name);
+            assertEquals(-1, getDifference(startingQuality[i], app.items[i].quality));
+        }
+    }
+
+    @Test
+    public void sulfurasHandler() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 80) };
+        int[] startingQuality = getStartingQuality(items);
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        for (int i=0; i < app.items.length; i++) {
+            assertEquals("Sulfuras, Hand of Ragnaros", app.items[i].name);
+            assertEquals(0, getDifference(startingQuality[i], app.items[i].quality));
+        }
+    }
+
+    @Test
+    public void backStagePassesHandler() {
+        Item[] items = new Item[] { 
+            new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20)
+        };
+        int[] startingQuality = getStartingQuality(items);
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        for (int i=0; i < app.items.length; i++) {
+            assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[i].name);
+            if(app.items[i].sellIn < 0) {
+                assertEquals(0, app.items[i].quality);
+            }
+            if(app.items[i].sellIn < 11 && app.items[i].sellIn > 5) {
+                assertEquals(2, getDifference(app.items[i].quality, startingQuality[i]));
+            }
+            if(app.items[i].sellIn > 0 && app.items[i].sellIn < 6) {
+                assertEquals(3, getDifference(app.items[i].quality, startingQuality[i]));
+            }
+        }
+    }
+
+    @Test
+    public void notOverMaxLimit() {
+        Item[] items = new Item[] { new Item("foo", 14, 50) };
+        int[] startingQuality = getStartingQuality(items);
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        for (int i=0; i < app.items.length; i++) {
+            assertTrue(app.items[i].quality <= MAX_QUALITY_VALUE);
+        }
     }
 
 }
